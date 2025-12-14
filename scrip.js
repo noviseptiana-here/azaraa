27a5f5f24e15593d72dc49da1e0085dd0377ffc7
noviseptiana-here <novi.septiana105@sma.belajar.id>
2025-12-14 10:37:43 +0700

    Add multiple tampilkan functions to scrip.js

---
+++ scrip.js
@@ -0,0 +1,41 @@
+function tampilkan(id) {
+    const semuaKonten = document.querySelectorAll(".konten");
+    semuaKonten.forEach(konten => {
+        konten.classList.remove("aktif");
+    });
+
+    document.getElementById(id).classList.add("aktif");
+}
+function tampilkan(id) {
+    const semuaKonten = document.querySelectorAll(".konten");
+    semuaKonten.forEach(konten => {
+        konten.classList.remove("aktif");
+    });
+
+    document.getElementById(id).classList.add("aktif");
+}
+function tampilkan(id) {
+    const semuaKonten = document.querySelectorAll(".konten");
+    semuaKonten.forEach(konten => {
+        konten.classList.remove("aktif");
+    });
+
+    document.getElementById(id).classList.add("aktif");
+}
+function tampilkan(id) {
+    const semuaKonten = document.querySelectorAll(".konten");
+    semuaKonten.forEach(konten => {
+        konten.classList.remove("aktif");
+    });
+
+    document.getElementById(id).classList.add("aktif");
+}
+function tampilkan(id) {
+    const semuaKonten = document.querySelectorAll(".konten");
+    semuaKonten.forEach(konten => {
+        konten.classList.remove("aktif");
+    });
+
+    document.getElementById(id).classList.add("aktif");
+}
+v
